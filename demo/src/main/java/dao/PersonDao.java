12dc6405c9db7bfb81501b383bbc73d5423e799c
Ryan Manchester <ryanm17@gmail.com>
2023-08-09 11:44:12 -0400

    Implements DAO interface

---
+++ demo/src/main/java/dao/PersonDao.java
@@ -0,0 +1,16 @@
+package dao;
+
+import java.util.UUID;
+
+import model.Person;
+
+public interface PersonDao {
+	
+	int insertPerson(UUID id, Person person);
+	
+	default int addPerson(Person person) {
+		UUID id = UUID.randomUUID();
+		return insertPerson(id, person);
+	}
+
+}
