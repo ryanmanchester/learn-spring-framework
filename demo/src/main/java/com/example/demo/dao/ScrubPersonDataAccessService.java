5b27b0c653498fd922403fc4459f06be8a434c3f
Ryan Manchester <ryanm17@gmail.com>
2023-08-09 13:34:59 -0400

    adds spring annotations

--- demo/src/main/java/com/example/demo/dao/ScrubPersonDataAccessService.java
+++ demo/src/main/java/com/example/demo/dao/ScrubPersonDataAccessService.java
@@ -4,8 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.stereotype.Repository;
+
 import com.example.demo.model.Person;
 
+
+@Repository("scrubDao")
 public class ScrubPersonDataAccessService implements PersonDao {
 
 		private static List<Person> DB = new ArrayList<>();
