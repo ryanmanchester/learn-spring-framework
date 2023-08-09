18a5c7b9a8c5c5c145ddc37526b1589adf723a8f
Ryan Manchester <ryanm17@gmail.com>
2023-08-09 12:02:15 -0400

    Implements service class

---
+++ demo/src/main/java/dao/ScrubPersonDataAccess.java
@@ -0,0 +1,17 @@
+package dao;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import model.Person;
+
+public class ScrubPersonDataAccess implements PersonDao {
+	private static List<Person> DB = new ArrayList<>();
+	
+	@Override
+	public int insertPerson(UUID id, Person person) {
+		DB.add(new Person(id, person.getName()));
+		return 0;
+	}
+}
---
+++ demo/src/main/java/service/PersonService.java
@@ -0,0 +1,16 @@
+package service;
+
+import dao.PersonDao;
+import model.Person;
+
+public class PersonService {
+	private final PersonDao personDao;
+	
+	public PersonService(PersonDao personDao) {
+		this.personDao = personDao;
+	}
+	public int addPerson(Person person) {
+		return personDao.insertPerson(person);
+	}
+
+}
