00a35c79c2f91ce48a1d851b56d93b08d969a205
Ryan Manchester <ryanm17@gmail.com>
2023-11-10 15:52:35 -0500

    sets up component and component scan annotations to create beans

--- learn-spring-framework/src/main/java/com/ryanmanchester/learnspringframework/App03GamingSpringBeans.java
+++ learn-spring-framework/src/main/java/com/ryanmanchester/learnspringframework/App03GamingSpringBeans.java
@@ -2,33 +2,24 @@ package com.ryanmanchester.learnspringframework;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import com.ryanmanchester.learnspringframework.game.GameInterface;
 import com.ryanmanchester.learnspringframework.game.GameRunner;
-import com.ryanmanchester.learnspringframework.game.PacMan;
 
 @Configuration
-class GamingConfig {
-	
-	@Bean
-	public GameInterface game() {
-		PacMan game = new PacMan();
-		return game;
-	}
+@ComponentScan("com.ryanmanchester.learnspringframework.game")
+public class App03GamingSpringBeans {
 	
 	@Bean
 	public GameRunner runner(GameInterface game) {
 		GameRunner runner = new GameRunner(game);
 		return runner;
 	}
-
-}
-
-public class App03GamingSpringBeans {
-
+	
 	public static void main(String[] args) {
-		try(AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(GamingConfig.class)) {
+		try(AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(App03GamingSpringBeans.class)) {
 			context.getBean(GameInterface.class).up();
 			context.getBean(GameRunner.class).run();
 		}
