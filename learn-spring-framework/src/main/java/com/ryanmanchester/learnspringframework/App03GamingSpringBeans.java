5ae2714986153c1cc758f20c839ca70f7b2a3f06
Ryan Manchester <ryanm17@gmail.com>
2023-11-10 15:41:39 -0500

    combines config file and app file, deletes config file

--- learn-spring-framework/src/main/java/com/ryanmanchester/learnspringframework/App03GamingSpringBeans.java
+++ learn-spring-framework/src/main/java/com/ryanmanchester/learnspringframework/App03GamingSpringBeans.java
@@ -1,10 +1,29 @@
 package com.ryanmanchester.learnspringframework;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import com.ryanmanchester.learnspringframework.game.GameInterface;
 import com.ryanmanchester.learnspringframework.game.GameRunner;
+import com.ryanmanchester.learnspringframework.game.PacMan;
 
+@Configuration
+class GamingConfig {
+	
+	@Bean
+	public GameInterface game() {
+		PacMan game = new PacMan();
+		return game;
+	}
+	
+	@Bean
+	public GameRunner runner(GameInterface game) {
+		GameRunner runner = new GameRunner(game);
+		return runner;
+	}
+
+}
 
 public class App03GamingSpringBeans {
 
