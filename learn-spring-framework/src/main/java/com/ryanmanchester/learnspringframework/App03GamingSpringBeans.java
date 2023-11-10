bfd58aabe9a1a2eda596a9402537073ff10485e9
Ryan Manchester <ryanm17@gmail.com>
2023-11-10 15:58:13 -0500

    more component examples to auto create beans

--- learn-spring-framework/src/main/java/com/ryanmanchester/learnspringframework/App03GamingSpringBeans.java
+++ learn-spring-framework/src/main/java/com/ryanmanchester/learnspringframework/App03GamingSpringBeans.java
@@ -1,26 +1,17 @@
 package com.ryanmanchester.learnspringframework;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-import com.ryanmanchester.learnspringframework.game.GameInterface;
 import com.ryanmanchester.learnspringframework.game.GameRunner;
 
 @Configuration
 @ComponentScan("com.ryanmanchester.learnspringframework.game")
 public class App03GamingSpringBeans {
 	
-	@Bean
-	public GameRunner runner(GameInterface game) {
-		GameRunner runner = new GameRunner(game);
-		return runner;
-	}
-	
 	public static void main(String[] args) {
 		try(AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(App03GamingSpringBeans.class)) {
-			context.getBean(GameInterface.class).up();
 			context.getBean(GameRunner.class).run();
 		}
 
