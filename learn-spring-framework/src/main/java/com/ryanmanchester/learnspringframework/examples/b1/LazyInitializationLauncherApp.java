cf4cd971e7aebe2874ef1f79c876b9941a53a66b
Ryan Manchester <ryanm17@gmail.com>
2023-11-13 17:52:28 -0500

    lazy initialization concept coded

--- learn-spring-framework/src/main/java/com/ryanmanchester/learnspringframework/examples/b1/LazyInitializationLauncherApp.java
+++ learn-spring-framework/src/main/java/com/ryanmanchester/learnspringframework/examples/b1/LazyInitializationLauncherApp.java
@@ -5,6 +5,27 @@ import java.util.Arrays;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+@Component
+class ClassA{
+	
+}
+
+@Component
+@Lazy
+class ClassB {
+	private ClassA classA;
+	public ClassB(ClassA classA) {
+		System.out.println("Initialization Logic");
+		this.classA = classA;
+	}
+	
+	public void doSomething() {
+		System.out.println("Do something here");
+	}
+}
 
 
 @Configuration
@@ -13,9 +34,8 @@ public class LazyInitializationLauncherApp {
 	
 	public static void main(String[] args) {
 		try(AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(LazyInitializationLauncherApp.class)) {
-			Arrays.stream(context.getBeanDefinitionNames())
-				.forEach(System.out::println);
-			
+			System.out.println("Initialization of context is complete");
+			context.getBean(ClassB.class).doSomething();
 		}
 
 	}
